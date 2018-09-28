1c4430ae29ffec4d37e325b4dad3c908e8fb9ad5
aliugenb <yeliu89@sina.com>
2018-09-28 18:05:26 +0800

    ...

--- apptest/views/bugstatistics.py
+++ apptest/views/bugstatistics.py
@@ -1,12 +1,29 @@
 # Created by liuye at 2018/9/14
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from bugstatistics.models import BUG
 
 
 def bugstatistics(request):
     bug_list = BUG.objects.all()
-    return render(request, 'bugstatistics/index.html', {'bugs':bug_list})
+    return render(request, 'bugstatistics/index.html', {'bugs': bug_list})
 
 
+@csrf_exempt
 def add_bug(request):
-    return render(request, 'bugstatistics/add_bug.html')
+    post_response = {'ret': 1, 'msg': 'post success!!'}
+    get_response = {'error': '使用post请求!!'}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        print(title)
+        iteration = request.POST.get('iteration')
+        print(iteration)
+        version = request.POST.get('version')
+        print(version)
+        status = request.POST.get('status')
+        print(status)
+        BUG.objects.create(title=title, iteration=iteration, version=version, status=status)
+        return JsonResponse(post_response)
+    if request.method == 'GET':
+        return JsonResponse(get_response)
